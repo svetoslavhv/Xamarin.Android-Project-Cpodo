768ed899c8c8f0f87098de712790534421a070e3
Svetoslav Hlebarov <svetoslav.s.hlebarov@gmail.com>
2019-12-22 09:19:06 +0100

    ContactInformationActivity : - Functionality added to buttons : "La ciudad" , "El palacio"

--- Cpodo/Activities/ContactInformationActivity.cs
+++ Cpodo/Activities/ContactInformationActivity.cs
@@ -16,17 +16,34 @@ namespace Cpodo.Activities
 	[Activity(Label = "ContactInformationActivity", ScreenOrientation = ScreenOrientation.Portrait)]
 	public class ContactInformationActivity : Activity
 	{
+		ImageButton cityImageBtn;
+		ImageButton palaceImageBtn;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
 			SetContentView(Resource.Layout.ContactInformationActivity);
-
+			
 			Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
 			toolbar.NavigationOnClick += delegate
 			{
 				StartActivity(typeof(MainActivity));
 				Finish();
+			};
+
+			cityImageBtn = FindViewById<ImageButton>(Resource.Id.cityImageBtn);
+			cityImageBtn.Click += delegate
+			{
+				//TODO: open a website
+			};
+
+			palaceImageBtn = FindViewById<ImageButton>(Resource.Id.palaceImageBtn);
+			palaceImageBtn.Click += delegate
+			{
+				var uri = Android.Net.Uri.Parse("https://www.palcongres-vlc.com/");
+				var intent = new Intent(Intent.ActionView, uri);
+				StartActivity(intent);
 			};
 		}
 	}
