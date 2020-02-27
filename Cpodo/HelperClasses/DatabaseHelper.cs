cb9b7e5073653e7453f749b97db7eb57efba81a1
Svetoslav Hlebarov <svetoslav.s.hlebarov@gmail.com>
2020-02-27 18:34:15 +0100

    DatabaseHelper created

---
+++ Cpodo/HelperClasses/DatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Cpodo.HelperClasses
+{
+	public static class DatabaseHelper
+	{
+		public static List<T> GetAllFromTable<T>() where T: new()
+		{
+			using (var connection = new SQLiteConnection(System.IO.Path.Combine(GlobalVariables.databasePath, "MobileSell.db")))
+			{
+				try
+				{
+					var all = connection.Table<T>().ToList();
+					return all;
+				}
+				catch (Exception ex)
+				{
+					return null;
+
+				}
+			}
+		}
+	}
+}
