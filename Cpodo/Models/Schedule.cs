23d4f2cf7aa6aa03ccab846d41530926ca5c89d4
Svetoslav Hlebarov <svetoslav.s.hlebarov@gmail.com>
2020-02-28 05:52:02 +0100

    Bind Schedule table to Schedule model

--- Cpodo/Models/Schedule.cs
+++ Cpodo/Models/Schedule.cs
@@ -9,19 +9,27 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
 
 namespace Cpodo.Models
 {
+	[Table("schedule")]
 	public class Schedule
 	{
+		[PrimaryKey]
+		[Column("id")]
 		public int Id { get; set; }
 
+		[Column("day")]
 		public string Day { get; set; }
 
+		[Column("hour")]
 		public string Hour { get; set; }
 
+		[Column("short_description")]
 		public string ShortDescription { get; set; }
 
+		[Column("long_description")]
 		public string LongDescription { get; set; }
 
 	}
